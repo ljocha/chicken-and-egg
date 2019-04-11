68bb47ccfbc0f0d4b5ac2d25f3d42c6e28208dff
Ales Krenek <ljocha@ics.muni.cz>
2019-04-11 11:15:03 +0000

    unchanged minim params and script extra

--- minim.sh
+++ minim.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+np=$1
+
+if [ -z "$np" ]; then
+	set -- $(grep '^processor' /proc/cpuinfo | tail -1)
+	np=$(($3 + 1))
+	set -- $(grep '^flags' /proc/cpuinfo | head -1)
+	while [ -n "$1" -a "$1" != ht ]; do shift; done
+	if [ "$1" = ht]; then np=$(($np / 2)); fi
+fi
+
+
 
 minone() {
 	in=$1
@@ -5,30 +18,24 @@ minone() {
 	base=$(basename $in .pdb)
 	
 #	echo Prepare topology
-	gmx pdb2gmx -f $base.pdb -o $base.gro -p $base -i $base -water spce -ff amber99 -ignh
-	
+	gmx pdb2gmx -f $base.pdb -o $base.gro -p $base -i $base -water spce -ff amber99 -ignh &&
 #	echo Add box
-	gmx editconf -f $base.gro -o $base-box.gro -d 1.0 -bt cubic
-	
+	gmx editconf -f $base.gro -o $base-box.gro -d 1.0 -bt cubic &&
 	# not solvating
-	
 	# no ions
-	
 #	echo Minimize
-	gmx grompp -f minim.mdp -c $base-box.gro -p $base.top -o $base-min.tpr -po $base-min.mdp
-	gmx mdrun -v -deffnm $base-min
+	gmx grompp -f minim.mdp -c $base-box.gro -p $base.top -o $base-min.tpr -po $base-min.mdp &&
+	gmx mdrun -v -deffnm $base-min || exit 1
 }
-
-nproc=4
 
 nfil=$(ls conf*.pdb | wc -l)
 
-for p in $(seq 1 $nproc); do
+for p in $(seq 1 $np); do
 	(
 		i=$p
 		while [ $i -le $nfil ]; do
 			minone conf$i.pdb
-			i=$(($i + $nproc))
+			i=$(($i + $np))
 		done
 	) &
 done
