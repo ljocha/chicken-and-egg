c83945122bbb3f092bd3e1b8d3ad698652449efc
Ales Krenek <ljocha@ics.muni.cz>
2019-04-15 11:15:16 +0000

    singularity in docker

---
+++ singularity/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+IMAGE=${CI_REGISTRY_IMAGE:-ljocha}
+
+docker build -t ljocha/singularity singularity &&
+docker run -v $PWD/singularity:/mnt ljocha/singularity "singularity build -w chicken-and-egg.img docker:$IMAGE/chicken-and-egg"
