f11e98de16f86a5ba75f8d4c5b735463947ae262
Ales Krenek <ljocha@ics.muni.cz>
2019-06-11 12:43:51 +0000

    non-default port

--- run.sh
+++ run.sh
@@ -2,13 +2,15 @@
 
 image=ljocha/chicken-and-egg:latest
 
+port=${1:-9000}
+
 docker=docker
 which nvidia-docker && docker=nvidia-docker
 
 # $docker run -p 9000:9000 -v $PWD:/home/$USER/MOUNTED ljocha/chicken-and-egg:latest bash -c "source /opt/intelpython3/bin/activate && jupyter notebook --ip 0.0.0.0 --port 9000"
 
-id=$($docker run -e OMP_NUM_THREADS -u 0 -p 9000:9000 -d -v $PWD:/home/jupyter/MOUNTED $image bash -c "chown $(id -u) /home/jupyter; while true; do sleep 365d; done")
+id=$($docker run -e OMP_NUM_THREADS -u 0 -p $port:$port -d -v $PWD:/home/jupyter/MOUNTED $image bash -c "chown $(id -u) /home/jupyter; while true; do sleep 365d; done")
 
 trap "$docker kill $id" EXIT
 
-$docker exec -u $(id -u) $id bash -c "source /opt/intelpython3/bin/activate && jupyter notebook --ip 0.0.0.0 --port 9000"
+$docker exec -u $(id -u) $id bash -c "source /opt/intelpython3/bin/activate && jupyter notebook --ip 0.0.0.0 --port $port"
