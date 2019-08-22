befb0d44d021f55ea82a9aa3c66f15c7fd505f98
Ales Krenek <ljocha@ics.muni.cz>
2019-08-22 12:55:24 +0000

    variable boxes, checking minimization results

--- minim.sh
+++ minim.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 np=$1
+box=${2:-1.0}
 
 if [ -z "$np" ]; then
 	echo usage: $0 number_of_cores
@@ -18,12 +19,14 @@ minone() {
 #	echo Prepare topology
 	gmx pdb2gmx -f $base.pdb -o $base.gro -p $base -i $base -water spce -ff amber99 -ignh &&
 #	echo Add box
-	gmx editconf -f $base.gro -o $base-box.gro -d 1.0 -bt cubic &&
+	gmx editconf -f $base.gro -o $base-box.gro -d $box -bt cubic &&
 	# not solvating
 	# no ions
 #	echo Minimize
 	gmx grompp -f minim.mdp -c $base-box.gro -p $base.top -o $base-min.tpr -po $base-min.mdp -maxwarn 1 &&
-	gmx mdrun -v -deffnm $base-min -ntmpi 1 -ntomp 1 || exit 1
+	gmx mdrun -v -deffnm $base-min -ntmpi 1 -ntomp 1 &&
+	(echo 10; echo) | gmx energy -f $base-min.edr -xvg none -o $base.x &&
+	tail -1 $base.x $base.minen && rm $base.x
 }
 
 nfil=$(ls conf*.pdb | wc -l)
