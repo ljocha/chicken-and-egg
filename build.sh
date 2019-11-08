d8cbd7d99bc5bbe11895e80327d3630b751fb622
Ales Krenek <ljocha@ics.muni.cz>
2019-11-08 17:43:16 +0000

    oops

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 INTELPYTHON=l_pythoni3_p_2019.4.088.tar.gz
 IMAGE=ljocha/chicken-and-egg
-VERSION=2019.11.8-1
+VERSION=:2019.11.8-1
 
 if [ ! -f "$INTELPYTHON" ]; then
 	echo Intel Python distribution is required, download from https://software.intel.com/en-us/distribution-for-python first and copy the tarball here. >&2
