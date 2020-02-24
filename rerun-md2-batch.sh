a84215d33f6b04b75daa08a706ee793017781e69
Ales Krenek <ljocha@ics.muni.cz>
2020-02-24 17:27:08 +0100

    right workdir

--- rerun-md2-batch.sh
+++ rerun-md2-batch.sh
@@ -9,6 +9,7 @@ trap "cp md3.* $workdir" EXIT
 NTMPI=$(($PBS_NUM_PPN / 2))
 export NTMPI
 
+cd $PBS_O_WORKDIR &&
 cp md.mdp npt.gro md2.cpt topol.top plumed.dat reference.pdb COLVAR HILLS $SCRATCHDIR &&
 cd $SCRATCHDIR &&
 $base/rerun-md2.sh
