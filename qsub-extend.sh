5d2756848dbf362cb631024325aeff5dcf454b22
Ales Krenek <ljocha@ics.muni.cz>
2020-04-09 19:32:29 +0200

    adapt to convert-tpr in notebook and -cpi in extend jobs

--- qsub-extend.sh
+++ qsub-extend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ncpus=32
+ncpus=40
 ntomp=4
 ngpus=2
 
@@ -9,52 +9,47 @@ num=$1
 name=$(basename $PWD)
 basedir=$PWD
 
+prefix=/storage/brno3-cerit/home/ljocha/work/chicken-and-egg/
+
 ntmpi=$(($ncpus / $ntomp))
 
-grompp="/storage/brno3-cerit/home/ljocha/work/chicken-and-egg/gmx-docker -p -- grompp"
-mdrun="/storage/brno3-cerit/home/ljocha/work/chicken-and-egg/gmx-docker -p -n $ntmpi -- mdrun"
+grompp="${prefix}/gmx-docker -p -- grompp"
+mdrun="${prefix}/gmx-docker -p -n $ntmpi -- mdrun"
+
+echo RESTART >plumed-restart.dat
+cat plumed.dat >>plumed-restart.dat
 
 prevjob=
-for i in $(seq 2 $num); do
+for i in $(seq 1 $num); do
 	n=$(printf '%02d' $i)
-	dir=md2-$n
-
-	i1=$(($i - 1))
-	n1=$(printf '%02d' $i1)
-	prevdir=md2-$n1
+	prev=$(printf '%02d' $(($i - 1)) )
 
 	script=$name-md2-$n.sh
 	
-	mkdir $dir
-	cp md.mdp npt.gro topol.top reference.pdb $dir
-
-	echo RESTART >$dir/plumed.dat
-	cat plumed.dat >>$dir/plumed.dat
-
-	cat - >$dir/$script <<EOF
+	cat - >$script <<EOF
 #!/bin/bash
 
-trap "cp COLVAR HILLS md2.* $basedir/$dir" EXIT
+trap "cp COLVAR HILLS md2.* $basedir" EXIT
 
-cd $basedir/$prevdir
-cp md2.cpt COLVAR HILLS \$SCRATCHDIR
-cd $basedir/$dir
-cp md.mdp npt.gro topol.top plumed.dat reference.pdb \$SCRATCHDIR
+cd $basedir
+mkdir md2-$prev
+cp reference.pdb plumed-restart.dat md2.* COLVAR HILLS \$SCRATCHDIR
+
+mv COLVAR HILLS md2.* md2-$prev
 
 cd \$SCRATCHDIR
 unset OMP_NUM_THREADS
 
-$grompp -f md.mdp -c npt.gro -t md2.cpt -p topol.top -o md2.tpr &&
-$mdrun -ntomp $ntomp -pin on -deffnm md2 -plumed plumed.dat
+$mdrun -ntomp $ntomp -pin on -deffnm md2 -cpi md2.cpt -plumed plumed-restart.dat
 
 EOF
 
-	chmod +x $dir/$script
+	chmod +x $script
 
 	if [ -n "$prevjob" ]; then dep="-W depend=afterany:$prevjob"; else dep=""; fi
 
 #	prevjob="fake-$n"
-	prevjob=$(qsub -q gpu -l walltime=24:0:0 -l select=1:ncpus=$ncpus:ngpus=$ngpus:mem=60GB:scratch_local=200GB $dep $dir/$script)
+	prevjob=$(qsub -q gpu -l walltime=24:0:0 -l select=1:cluster=glados:ncpus=$ncpus:ngpus=$ngpus:mem=60GB:scratch_local=200GB $dep $script)
 	echo $script: $prevjob
 done
 echo Good luck!
