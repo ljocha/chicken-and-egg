2a596c2a65814c0425dfb8a1ce59b3720993ab63
Ales Krenek <ljocha@ics.muni.cz>
2022-01-03 16:44:15 +0100

    install anncolvar with pip; giveup intelpython; bump version

--- build.sh
+++ build.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-INTELPYTHON=l_pythoni3_p_2020.0.014.tar.gz
 source ./docker-image.sh
 VERSION=:$(cat VERSION)
 
-if [ ! -f "$INTELPYTHON" ]; then
-	echo Intel Python distribution is required, download from https://software.intel.com/en-us/distribution-for-python first and copy the tarball here. >&2
-	echo Change the filename \($INTELPYTHON\) in this script eventually. >&2
-	exit 1
-fi
-
-# docker build --pull -t $IMAGE$VERSION --build-arg INTELPYTHON=${INTELPYTHON} .
-docker build -t $IMAGE$VERSION --build-arg INTELPYTHON=${INTELPYTHON} . &&
+docker build -t $IMAGE$VERSION . &&
 docker tag $IMAGE$VERSION $IMAGE:latest &&
 docker push $IMAGE$VERSION &&
 docker push $IMAGE:latest
